f4ca3d9f13310eecaefeef19f62e5b98eb61fb0c
Ricardo Altmann <tenaltmann@gmail.com>
2025-02-18 01:10:58 -0300

    Termino da criação das variaveis ambientes e instalação do biome como formatador

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
-import { fastify } from "fastify";
 import { fastifyCors } from '@fastify/cors'
-import {
-    validatorCompiler,
-    serializerCompiler,
-    ZodTypeProvider,
-    jsonSchemaTransform,
-} from 'fastify-type-provider-zod'
 import { fastifySwagger } from '@fastify/swagger'
 import { fastifySwaggerUi } from '@fastify/swagger-ui'
-import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
+import { fastify } from 'fastify'
+import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { env } from './env'
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -18,21 +19,21 @@ app.setValidatorCompiler(validatorCompiler)
 app.register(fastifyCors)
 
 app.register(fastifySwagger, {
-    openapi: {
-        info: {
-            title: 'NLW Connect',
-            version: '0.0.1',
-        },
+  openapi: {
+    info: {
+      title: 'NLW Connect',
+      version: '0.0.1',
     },
-    transform: jsonSchemaTransform,
+  },
+  transform: jsonSchemaTransform,
 })
 
 app.register(fastifySwaggerUi, {
-    routePrefix: '/docs',
+  routePrefix: '/docs',
 })
 
 app.register(subscribeToEventRoute)
 
-app.listen({port: 3333}).then(() => {
-    console.log('HTTP server runnig!')
+app.listen({ port: env.PORT }).then(() => {
+  console.log('HTTP server runnig!')
 })
